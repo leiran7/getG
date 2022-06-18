e56fe00f5f0222150cf251fe96e312e8f225e229
leran sobih <scolern7@gmail.com>
2022-06-18 17:05:52 +0300

    added the ability tow add several employees at the same time

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,38 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addEmployee = functions.https.onRequest(async (request, response) => {
-    if(!request.body.profileURL || ! request.body.friends){
-        response.status(400).end("profileURL and friends fields should be included in the data");
-    }
+  if (
+    !request.body.hasOwnProperty("profileURL") ||
+    !request.body.hasOwnProperty("friends")
+  ) {
+    response
+      .status(400)
+      .end("profileURL and friends fields should be included in the data");
+  }
 
-    let docRef = admin.firestore().collection("persons").doc(body.profileURL);
-    await docRef.set({
-      ...request.body
+  let docRef = admin.firestore().collection("employees").doc(body.profileURL);
+  await docRef.set({
+    ...request.body,
+  });
+
+  response.status(200).end();
+});
+
+exports.addEmployees = functions.https.onRequest(async (request, response) => {
+  let employeesColRef = admin.firestore().collection("employees");
+  if (!request.body.hasOwnProperty("employeesList")) {
+    response
+      .status(400)
+      .end(
+        "employeesList of type array fields should be included in the data "
+      );
+  }
+
+  for (const employee of request.body.employeesList) {
+    await employeesColRef.doc(employee.profileURL).set({
+      ...employee,
     });
+  }
 
-    response.status(200).end();
+  response.status(200).end();
 });
