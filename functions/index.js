263a026851d0f8a846bb42ff79f12d60941d8787
leran sobih <scolern7@gmail.com>
2022-06-18 16:50:17 +0300

    added addEmploee function

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,15 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.addCompany = functions.https.onRequest(async (request, response) => {
-  console.log("company added successfully");
-  switch (request.get("content-type")) {
-    case "application/json":
-      console.log(request.body);
-      break;
-  }
+exports.addEmployee = functions.https.onRequest(async (request, response) => {
+    if(!request.body.profileURL || ! request.body.friends){
+        response.status(400).end("profileURL and friends fields should be included in the data");
+    }
 
-  let docRef = admin.firestore().collection("companies").doc("aa");
-  await docRef.set({
-    aa: 5,
-  });
+    let docRef = admin.firestore().collection("persons").doc(body.profileURL);
+    await docRef.set({
+      ...request.body
+    });
 
-  response.end();
-});
-
-exports.addEmployee = functions.https.onRequest((request, response) => {
-  console.log("employee added successfully");
-  response.end();
+    response.status(200).end();
 });
